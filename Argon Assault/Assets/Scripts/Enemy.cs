a934af17a153430411841a6b221d24a4091c485b
bhixsonsimeral16 <brian.hixsonsimeral@gmail.com>
2023-04-07 00:13:23 -0400

    Add new enemy prefabs. Start enemy wave creation. Use tags to hide unnecessary objects and delete them.

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -3,29 +3,56 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 10;
+    [SerializeField] float maxHitPoints = 100f;
+    [SerializeField] float currentHitPoints = 100f;
     ScoreBoard scoreBoard;
+    Renderer renderer;
+    GameObject parentGameObject;
     void Start()
     {
+        renderer = GetComponent<Renderer>();
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddRigidBody();
     }
 
-    private void OnParticleCollision(GameObject other)
+    void AddRigidBody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if (gameObject.tag == "BlankEnemy")
+        {
+            ChangeColor();
+        }
+        if (currentHitPoints <= 0)
+        {
+            KillEnemy();
+        }
     }
 
     void ProcessHit()
     {
+        currentHitPoints -= 10;
         scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void ChangeColor()
+    {
+        float healthPercentage = currentHitPoints / maxHitPoints;
+        Color newColor = Color.Lerp(Color.white, Color.red, 1 - healthPercentage);
+        renderer.material.color = newColor;
     }
 
     void KillEnemy()
     {
         GameObject vfx = Instantiate(deathFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 }
